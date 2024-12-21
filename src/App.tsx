06e180d0f87af81686db701b050e7662cbc24918
Tejv1708 <narukatejveer@gmail.com>
2024-12-21 11:53:48 +0530

    This is for polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { useRef } from "react";
 import Button from "./components/Button";
+import Input from "./components/Input";
+
+// ! If I am using the ref from my custom component without using forwardRef() function then i got the error
+// * Waring -> Warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <>
       <main>
-        <p>
-          <Button>A Button</Button>
-        </p>
-        <p>
-          <Button href="https://google.com">A Link</Button>
-        </p>
+        <Input label="Text" id="text" ref={inputRef} />
       </main>
     </>
   );
