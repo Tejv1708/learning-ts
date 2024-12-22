eb64f14bda37ad9915437e4b833b2a6bb1626937
Tejv1708 <narukatejveer@gmail.com>
2024-12-22 10:18:32 +0530

    How to use useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { useRef } from "react";
-import Button from "./components/Button";
-import Input from "./components/Input";
+import Button from "./components/UI/Button";
+import Input from "./components/UI/Input";
+import Form, { type FormHandle } from "./components/UI/Form";
 
 // ! If I am using the ref from my custom component without using forwardRef() function then i got the error
 // * Waring -> Warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
+  // const inputRef = useRef<HTMLInputElement>(null);
   return (
     <>
       <main>
-        <Input label="Text" id="text" ref={inputRef} />
+        <Form onSave={handleSave} ref={customForm}>
+          <Input label="Name" type="text" id="name" />
+          <Input label="Age" type="number" id="age" />
+          <p>
+            <Button>Save</Button>
+          </p>
+        </Form>
       </main>
     </>
   );
---
+++ src/components/UI/AddTimer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddTimer = () => {
+  return <div>AddTimer</div>;
+};
